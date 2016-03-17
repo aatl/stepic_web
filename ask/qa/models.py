a1d6f6833ef3817a94dc5a0296beb22367e05a72
desh_ <shinobispb@gmail.com>
2016-03-17 06:00:00 +0400

    qa.models datatime fix

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(null=True)
     author = models.ForeignKey(User, related_name='question_user')
     likes = models.ManyToManyField(User, related_name='likes_user')
 
@@ -15,6 +15,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, null=False)
     author = models.ForeignKey(User)
